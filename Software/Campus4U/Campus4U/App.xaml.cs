a2a653839e9470873a567685826ee085c8b3f5c8
lkanjir23 <lkanjir23@student.foi.hr>
2025-12-15 16:38:58 +0100

    Kreirani projekti

---
+++ Software/Campus4U/Campus4U/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Campus4U
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
