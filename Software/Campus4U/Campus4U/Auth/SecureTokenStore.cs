1fc1fcba942b61a3362990808dbd7f6b7c91f952
lkanjir23 <lkanjir23@student.foi.hr>
2025-12-18 16:58:20 +0100

    #26 - Poboljšana pohrana tokena Implementirano ispravno ponašanje kod čitanja tokena s diska (prema #28), ako je: - datoteka prazna - dekripcija ne uspije - deserijalizacija ne uspije

--- Software/Campus4U/Campus4U/Auth/SecureTokenStore.cs
+++ Software/Campus4U/Campus4U/Auth/SecureTokenStore.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
@@ -11,6 +12,7 @@ public sealed record Token(
     DateTimeOffset? ExpiresAt
 );
 
+//Luka Kanjir
 public sealed class SecureTokenStore
 {
     private readonly string path =
@@ -22,22 +24,44 @@ public sealed class SecureTokenStore
 
     public async Task SaveAsync(Token token)
     {
-        Directory.CreateDirectory(Path.GetDirectoryName(path));
+        var directory = Path.GetDirectoryName(path);
+        if (!string.IsNullOrWhiteSpace(directory)) Directory.CreateDirectory(directory);
+
         var json = JsonSerializer.Serialize(token);
         var bytes = ProtectedData.Protect(Encoding.UTF8.GetBytes(json), null, DataProtectionScope.CurrentUser);
         await File.WriteAllBytesAsync(path, bytes);
     }
 
-    public async Task<Token> ReadAsync()
+    public async Task<Token?> ReadAsync()
     {
-        if(!File.Exists(path)) return null;
+        if (!File.Exists(path)) return null;
         var bytes = await File.ReadAllBytesAsync(path);
-        var data = ProtectedData.Unprotect(bytes, null, DataProtectionScope.CurrentUser);
-        return JsonSerializer.Deserialize<Token>(Encoding.UTF8.GetString(data));
+        if (bytes.Length == 0)
+        {
+            Clear();
+            return null;
+        }
+
+        try
+        {
+            var data = ProtectedData.Unprotect(bytes, null, DataProtectionScope.CurrentUser);
+            return JsonSerializer.Deserialize<Token>(Encoding.UTF8.GetString(data));
+        }
+        catch (CryptographicException)
+        {
+            Clear();
+            return null;
+        }
+        catch (JsonException)
+        {
+            Clear();
+            return null;
+        }
+        
     }
 
     public void Clear()
     {
-        if(File.Exists(path)) File.Delete((path));
+        if (File.Exists(path)) File.Delete(path);
     }
 }
