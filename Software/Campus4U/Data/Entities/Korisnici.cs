a8eb11c4b911be9fddb1eaa8c1994aa5d50f1f2b
lkanjir23 <lkanjir23@student.foi.hr>
2025-12-29 23:26:04 +0100

    #31 - Povezana baza i dodane potrebne tablice za korisnika i uloge

---
+++ Software/Campus4U/Data/Entities/Korisnici.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Client.Data.Entities;
+
+public partial class Korisnici
+{
+    public int Id { get; set; }
+
+    public int UlogaId { get; set; }
+
+    public string Sub { get; set; } = null!;
+
+    public string? Ime { get; set; }
+
+    public string? Prezime { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? BrojSobe { get; set; }
+
+    public virtual Uloge Uloga { get; set; } = null!;
+}
