81651a72632e00efd70d76dcb62c01ab1d304224
nkihas23 <nkihas23@student.foi.hr>
2026-01-30 20:10:27 +0100

    Naprevljene template kartice za prikaz događaja. Također napravljena je klasa u koju se spremaju podaci za prikaz na kartici.(#19)

---
+++ Software/Campus4U/Campus4U/Views/Templates/EventCard.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Client.Presentation.Views.Templates
+{
+    /// <summary>
+    /// Interaction logic for EventCard.xaml
+    /// </summary>
+    public partial class EventCard : UserControl
+    {
+        private static readonly ImageSource DefaultImage =
+            new BitmapImage(new Uri("pack://application:,,,/Images/no-img.png", UriKind.Absolute));
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(EventCard), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty DateProperty =
+            DependencyProperty.Register(nameof(Date), typeof(string), typeof(EventCard), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register(nameof(Description), typeof(string), typeof(EventCard), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register(nameof(Image), typeof(ImageSource), typeof(EventCard), new PropertyMetadata(DefaultImage));
+
+        public EventCard()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public string Date
+        {
+            get => (string)GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        }
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public ImageSource Image
+        {
+            get => (ImageSource)GetValue(ImageProperty);
+            set => SetValue(ImageProperty, value);
+        }
+
+    }
+}
