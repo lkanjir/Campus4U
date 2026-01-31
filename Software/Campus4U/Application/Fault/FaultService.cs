d2a5e0f03b9dc802d7f7cc49e95aa960b0d485c7
Tin <tposavec23@student.foi.hr>
2026-01-31 19:55:55 +0100

    Dodane metode za dohvat, filtriranje, ažuriranje statusa i statistiku kvarova u Data i Application sloju. #98

--- Software/Campus4U/Application/Fault/FaultService.cs
+++ Software/Campus4U/Application/Fault/FaultService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Client.Application.Spaces;
+﻿using Client.Application.Spaces;
 using Client.Domain.Fault;
 using Client.Domain.Spaces;
 
@@ -58,6 +53,70 @@ namespace Client.Application.Fault
             );
 
             return await _kvarRepo.SpremiKvar(kvar);
+        }
+
+        // FZ-11
+        private static readonly List<string> _sviStatusi = new() { "Aktivan", "U obradi", "Riješen" };
+
+        public async Task<List<FaultReport>> DohvatiSveKvarove()
+        {
+            return await _kvarRepo.DohvatiSveKvarove();
+        }
+
+        public async Task<FaultReport?> DohvatiKvarPoId(int kvarId)
+        {
+            if (kvarId <= 0)
+                throw new ArgumentException("Neispravan ID kvara.", nameof(kvarId));
+
+            return await _kvarRepo.DohvatiKvarPoId(kvarId);
+        }
+
+        public async Task<List<FaultReport>> DohvatiKvaroveFiltrirano(string? status, int? prostorId, int? vrstaKvaraId, DateTime? odDatuma, DateTime? doDatuma)
+        {
+            if (odDatuma.HasValue && doDatuma.HasValue && odDatuma > doDatuma)
+                throw new ArgumentException("Datum 'od' ne može biti veći od datuma 'do'.");
+
+            return await _kvarRepo.DohvatiKvaroveFiltrirano(status, prostorId, vrstaKvaraId, odDatuma, doDatuma);
+        }
+
+        public async Task<bool> PromijeniStatusKvara(int kvarId, string noviStatus)
+        {
+            if (kvarId <= 0)
+                throw new ArgumentException("Neispravan ID kvara.", nameof(kvarId));
+
+            if (string.IsNullOrWhiteSpace(noviStatus))
+                throw new ArgumentException("Status ne može biti prazan.", nameof(noviStatus));
+
+            var kvar = await _kvarRepo.DohvatiKvarPoId(kvarId);
+            if (kvar == null)
+                throw new ArgumentException("Kvar ne postoji.", nameof(kvarId));
+
+            var dozvoljeni = DohvatiDozvoljeneStatuse(kvar.Status);
+            if (!dozvoljeni.Contains(noviStatus))
+                throw new InvalidOperationException($"Nije dozvoljen prijelaz iz '{kvar.Status}' u '{noviStatus}'.");
+
+            return await _kvarRepo.AzurirajStatusKvara(kvarId, noviStatus);
+        }
+
+        public List<string> DohvatiSveStatuse()
+        {
+            return _sviStatusi;
+        }
+
+        public List<string> DohvatiDozvoljeneStatuse(string trenutniStatus)
+        {
+            return trenutniStatus switch
+            {
+                "Aktivan" => new List<string> { "U obradi", "Riješen" },
+                "U obradi" => new List<string> { "Riješen" },
+                "Riješen" => new List<string>(),
+                _ => new List<string>()
+            };
+        }
+
+        public async Task<FaultStatistics> DohvatiStatistiku()
+        {
+            return await _kvarRepo.DohvatiStatistiku();
         }
     }
 }
