d7c94c12e86b5e40de9e67308fbe890def87199b
lkanjir23 <lkanjir23@student.foi.hr>
2026-02-01 21:14:42 +0100

    #106 - Dodane klijentske metode za slike događaja

--- Software/Campus4U/Application/Images/IImageService.cs
+++ Software/Campus4U/Application/Images/IImageService.cs
@@ -6,4 +6,7 @@ public interface IImageService
     Task<ImagePayload?> GetProfileImageAsync(int userId, CancellationToken ct = default);
     Task UploadProfileImageAsync(int userId, ImageUpload upload, CancellationToken ct = default);
     void InvalidateProfile(int userId);
+    Task<ImagePayload?> GetEventImageAsync(int eventId, CancellationToken ct = default);
+    Task UploadEventImageAsync(int eventId, ImageUpload upload, CancellationToken ct = default);
+    void InvalidateEvent(int eventId);
 }
