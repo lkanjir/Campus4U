652df7fa5f9f92c5e1b060b3e169e5a689c66ca8
lkanjir23 <lkanjir23@student.foi.hr>
2026-02-02 12:54:20 +0100

    Implementirana podrška za upload slika kvarova na klijentu

--- Software/Campus4U/Application/Images/IImageService.cs
+++ Software/Campus4U/Application/Images/IImageService.cs
@@ -9,4 +9,7 @@ public interface IImageService
     Task<ImagePayload?> GetEventImageAsync(int eventId, CancellationToken ct = default);
     Task UploadEventImageAsync(int eventId, ImageUpload upload, CancellationToken ct = default);
     void InvalidateEvent(int eventId);
+    Task<ImagePayload?> GetFaultImageAsync(int faultId, CancellationToken ct = default);
+    Task UploadFaultImageAsync(int faultId, ImageUpload upload, CancellationToken ct = default);
+    void InvalidateFault(int faultId);
 }
